e2b050682750542528c62f7c5894f2c355300cd1
Anastasios Poursaitides <tasos.poursaitidis.4@gmail.com>
2020-11-27 19:57:00 +0200

    Add functionality to ouput quote and author in the console

--- index.js
+++ index.js
@@ -8,3 +8,18 @@ const quotes = quotesObject.quotes;
 
 //Gives a random number from 0 to the last index of the array.
 const randomIndex = () => Math.floor(Math.random() * quotes.length);
+
+
+//Returns a random quote, formatted with a template literal
+function outputQuote() {
+  const index = randomIndex();
+  const quote = quotes[index].quote;
+  const output = `
+-- ${quotes[index].quote} --
+
+By: ${quotes[index].author}
+  `;
+  return output;
+}
+
+console.log(outputQuote())
