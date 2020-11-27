0071273759b06ae538bb05395463a6585a2257dd
Anastasios Poursaitides <tasos.poursaitidis.4@gmail.com>
2020-11-27 18:36:31 +0200

    Add 30 quotes to the quotes array

---
+++ data.js
@@ -0,0 +1,38 @@
+/*
+This file contains the neccessary data for the app
+to work. (Quotes)
+*/
+
+const quotes = [
+  {quote: "The great advantage about telling the truth is that nobody ever believes it.", author: "Dorothy L. Sayers"},
+  {quote: "I have no special talent. I am only passionately curious.", author: "Albert Einstein"},
+  {quote: "Wisely, and slow. They stumble that run fast", author: "William Shakespeare"},
+  {quote: "If you judge people, you have no time to love them", author: "Mother Teresa"},
+  {quote: "All that we are is the result of what we have thought", author: "Buddha"},
+  {quote: "The most courageous act is still to think for yourself. Aloud", author: "Coco Chanel"},
+  {quote: "Stay hungry, stay foolish", author: "Steve Jobs"},
+  {quote: "The greatest wealth is to live content with little", author: "Plato"},
+  {quote: "The future belongs to those who prepare for it today", author: "Malcolm X"},
+  {quote: "The successful warrior is the average man, with laser-like focus", author: "Bruce Lee"},
+  {quote: "A great man is always willing to be little", author: "Ralph Waldo Emerson"},
+  {quote: "Don’t count the days, make the days count", author: "Muhammad Ali"},
+  {quote: "In order to be irreplaceable one must always be different", author: "Coco Chanel"},
+  {quote: "Be kind whenever possible. It is always possible.", author: "Dalai Lama"},
+  {quote: "The root of suffering is attachment", author: "Buddha"},
+  {quote: "Well done is better than well said", author: "Benjamin Franklin"},
+  {quote: "It always seems impossible until it’s done.", author: "Nelson Mandela"},
+  {quote: "The unexamined life is not worth living.", author: "Socrates"},
+  {quote: "Our life is frittered away by detail… simplify, simplify.", author: "Henry David Thoreau"},
+  {quote: "Try not to become a man of success but rather to become a man of value.", author: "Albert Einstein"},
+  {quote: "The weak can never forgive. Forgiveness is the attribute of the strong.", author: "Mahatma Gandhi"},
+  {quote: "Turn your wounds into wisdom.", author: "Oprah Windrey"},
+  {quote: "I don’t think of all the misery but of the beauty that still remains.", author: "Anne Frank"},
+  {quote: "Life is a flower of which love is the honey.", author: "Victor Hugo"},
+  {quote: "Do what you can, with what you have, where you are.", author: "Theodore Roosevelt"},
+  {quote: "Little by little, one travels far.", author: "J.R.R Tolkien"},
+  {quote: "It’s not how much you have that makes people look up to you, it’s who you are.", author: "Elvis Presley"},
+  {quote: "I fear not the man who has practiced 10,000 kicks once, but I fear the man who has practiced one kick 10,000 times.", author: "Bruce Lee"},
+  {quote: "A man who stands for nothing will fall for anything.", author: "Malcolm X"},
+  {quote: "You have power over your mind – not outside events. Realize this, and you will find strength.", author: "Marcus Aurelius"},
+  {quote: "Early to bed and early to rise makes a man healthy, wealthy, and wise.", author: "Benjamin Franklin"},
+];
