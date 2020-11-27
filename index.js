20608c070dec47a211a8e32683bba3ec64fb8547
Anastasios Poursaitides <tasos.poursaitidis.4@gmail.com>
2020-11-27 19:09:37 +0200

    Add function to generate a random Int from 0 to the last element of the quotes array

--- index.js
+++ index.js
@@ -4,3 +4,7 @@
 const quotesObject = require('./data.js');
 //The above is an object so I have to .quotes to copy the actual array
 const quotes = quotesObject.quotes;
+
+
+//Gives a random number from 0 to the last index of the array.
+const randomIndex = () => Math.floor(Math.random() * quotes.length);
